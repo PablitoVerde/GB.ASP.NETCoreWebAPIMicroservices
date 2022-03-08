61645f83f3e11e66ff57ca5bec823ae22a2cfafa
Pablito Verde <pgreeny@mail.ru>
2022-03-08 16:48:25 +0300

    Менеджер метрик
    Добавление контроллеров и классов по методичке

---
+++ MetricsManager/MetricsManager/Controllers/AgentsController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace MetricsManager.Controllers
+{
+    // При компиляции путь, например, такой: api/agents/register
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AgentsController : ControllerBase
+    {
+
+        /// <summary>
+        /// Зарегистрировать агент метрик
+        /// </summary>
+        /// <param Имя="agentInfo"></param>
+        /// <returns></returns>
+        [HttpPost("register")]
+        public IActionResult RegisterAgent([FromBody] AgentInfo agentInfo)
+        {
+            return Ok();
+        }
+
+        /// <summary>
+        /// Активировать агент метрик
+        /// </summary>
+        /// <param Имя="agentId"></param>
+        /// <returns></returns>
+        [HttpPut("enable/{agentId}")]
+        public IActionResult EnableAgentById([FromRoute] int agentId)
+        {
+            return Ok();
+        }
+
+        /// <summary>
+        /// Дезактивировать агент метрик
+        /// </summary>
+        /// <param name="agentId"></param>
+        /// <returns></returns>
+        [HttpPut("disable/{agentId}")]
+        public IActionResult DisableAgentById([FromRoute] int agentId)
+        {
+            return Ok();
+        }
+
+    }
+}
---
+++ MetricsManager/MetricsManager/Controllers/RamMetricsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MetricsManager.Controllers
+{
+    [Route("api/metrics/ram")]
+    [ApiController]
+    public class RamMetricsController : ControllerBase
+    {
+    }
+}
